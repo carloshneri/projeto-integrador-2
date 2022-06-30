10152fd251be2836dfab3317a2806a03fd9e7afd
Carlos Neri <carloshneri@hotmail.com>
2022-06-29 23:09:28 -0300

    Aluno, livro e  emprestimo OK

--- app.py
+++ app.py
@@ -3,19 +3,15 @@ import requests
 import datetime
 
 ### GET/POST/DELETE/PUT - Aluno
-def get_alunos(url, alunos):
-    a = alunos  # remover essa linha com o Back
-    url = url  # remover essa linha com o Back
-    # alunos = requests.get(url + "/alunos/")
-    # return alunos.json()
-    return a  # remover essa linha com o Back
+def get_alunos(url):
+    alunos = requests.get(url + "usuario/")
+    return alunos.json()
 
 
-def post_aluno(url, dados):
-    # envio = requests.post(url + "/alunos/", data=dados)
-    alunos.append(dados)  # remover essa linha com o Back
-    # return envio
-    return ()  # remover essa linha com o Back
+def post_aluno(dados):
+    envio = requests.post("https://pi002.herokuapp.com/usuario/?format=json", data=dados)
+
+    return envio
 
 
 def delete_aluno():
@@ -24,7 +20,8 @@ def delete_aluno():
 
 def put_aluno(url, dados):
     id = str(dados["id"])
-    envio = requests.put(url + "/alunos/" + id, data=dados)
+    print(dados)
+    envio = requests.put(url + "usuario/" + id + "/?format=json", data=dados)
     return envio
 
 
@@ -71,49 +68,78 @@ def duvidas():
 @app.route("/aluno", methods=["GET", "POST"])
 def aluno():
     if request.method == "GET":
-        aluno = get_alunos(url, alunos)
-
+        pass
+        #aluno = requests.get("https://pi002.herokuapp.com/usuario/?format=json").json()
     if request.method == "POST":
-        al = {"id": request.form["input_aluno_id"], "nome": request.form["input_aluno_nome"]}
-        put_aluno(url, al)
-        aluno = get_alunos(url, aluno)
+        id = request.form["input_aluno_id"]
+        tipo = request.form["input_aluno_funcao"]
+        if tipo == "PUT":
+            al = {
+                "id": request.form["input_aluno_id"],
+                "ra": request.form["input_aluno_ra"],
+                "first_name": request.form["input_aluno_nome"],
+                "username": request.form["input_aluno_nome"],
+                "password": "3A",
+                "turma": request.form["input_aluno_turma"],
+                "ano": "4",
+                "date_joined": "2022-06-29T15:04:05.997621-03:00",
+            }
+        
+            print("https://pi002.herokuapp.com/usuario/" + id + "/?format=json")
+            requests.put("https://pi002.herokuapp.com/usuario/" + id + "/?format=json", data=al)
+           
+        
+        else:
+            url = "https://pi002.herokuapp.com/usuario/" + id + "/?format=json"
+            requests.delete(url)
+    
+
+    aluno = requests.get("https://pi002.herokuapp.com/usuario/?format=json").json()
     return render_template("alunos.html", alunos=aluno)
 
 
 @app.route("/cadastro_aluno", methods=["GET", "POST"])
 def aluno_envio():
-    if request.method == "POST":  # verifica se o metodo post do formulario
-        aluno = {"id": request.form["ra"], "nome": request.form["nome"]}
-        envio = post_aluno(url, aluno)
+    if request.method == "POST":
+        aluno = {
+            "ra": request.form["ra"],
+            "first_name": request.form["nome"],
+            "username": request.form["nome"],
+            "password": "3A",
+            "turma": request.form["serie"],
+            "ano": "4",
+            "date_joined": "2022-06-29T15:04:05.997621-03:00",
+        }
+        post_aluno(aluno)
     return render_template("aluno_cadastro.html")
 
 
 @app.route("/livros", methods=["GET", "POST"])
 def livros():
     if request.method == "POST":
-        
+
         tipo = request.form["input_livro_funcao"]
-        
+
         id = request.form["input_livro_id"]
-        
+
         if tipo == "DELETE":
             url = "https://pi002.herokuapp.com/livro/" + id + "/?format=json"
             requests.delete(url)
 
         elif tipo == "PUT":
-            
+
             l = {
                 "id": request.form["input_livro_id"],
                 "isbn": request.form["input_livro_isbn"],
                 "titulo": request.form["input_livro_titulo"],
-                 "autor": request.form["input_livro_autor"],
+                "autor": request.form["input_livro_autor"],
                 "editora": request.form["input_livro_editora"],
                 "assunto": request.form["input_livro_assunto"],
                 "resumo": request.form["input_livro_resumo"],
-                 }
+            }
             url = "https://pi002.herokuapp.com/livro/" + id + "/?format=json"
+            print(l)
             requests.put(url, l)
-        
 
     else:
         pass
@@ -122,32 +148,8 @@ def livros():
     return render_template("livros.html", livros=livros)
 
 
-
 @app.route("/devolucao")
 def devolucao():
-    """emprestimos = [
-        {
-            "id": "6",
-            "titulo": "tadsasdjhga",
-            "aluno": "Eu mesmo",
-            "data_de_emprestimo": "14/11/12",
-            "data_de_devolucao": "16/12/45",
-        },
-        {
-            "id": "7",
-            "titulo": "tadsasdjhga",
-            "aluno": "Eu mesmo",
-            "data_de_emprestimo": "15/07/12",
-            "data_de_devolucao": "12/12/45",
-        },
-        {
-            "id": "8",
-            "titulo": "tad",
-            "aluno": "Eu mesmo 001",
-            "data_de_emprestimo": "14/09/12",
-            "data_de_devolucao": "14/12/45",
-        },
-    ]"""
     emprestimos = get_devolucao()
     return render_template("devolucao.html", emprestimos=emprestimos)
 
@@ -155,6 +157,7 @@ def devolucao():
 @app.route("/cadastro_livro", methods=["GET", "POST"])
 def cadastro_livro():
     if request.method == "POST":
+
         l = {
             "id": request.form["isbn"],
             "isbn": request.form["isbn"],
@@ -166,7 +169,7 @@ def cadastro_livro():
             "resumo": request.form["resumo"],
             "data_cadastro": "2022-06-10",
         }
-
+        print(l["trumb"])
         # arquivo = request.form['capa_livro']
         envio = post_livro(l)
 
@@ -177,45 +180,30 @@ def cadastro_livro():
 
 @app.route("/emprestimo", methods=["GET", "POST"])
 def emprestimo():
-    
-    if request.method =="POST":
+
+    if request.method == "POST":
         complemento = "T18:33:36-03:00"
-        data_emprestimo = request.form['data_emprestimo']+complemento
+        data_emprestimo = request.form["data_emprestimo"] + complemento
         lista_data = list(data_emprestimo)
-        lista_data[4]="-"
+        lista_data[4] = "-"
         lista_data[7] = "-"
-        data_emprestimo = ''.join(lista_data)
-        print("essa é a lista ")        
+        data_emprestimo = "".join(lista_data)
+        print("essa é a lista ")
         print(data_emprestimo)
 
-        empr ={ "ra": int(request.form['ra']),
-                "dataemp": data_emprestimo,
-                "datadev": "2022-06-09T18:33:36-03:00",
-                "ativo":'True',
-                "isbn":int(request.form['isbn'])}
+        empr = {
+            "ra": int(request.form["ra"]),
+            "dataemp": data_emprestimo,
+            "datadev": "2022-06-09T18:33:36-03:00",
+            "ativo": "True",
+            "isbn": int(request.form["isbn"]),
+        }
         envio = requests.post("https://pi002.herokuapp.com/emprestimo/?format=json", empr)
         print("enviado o emprestimo")
         print(envio)
 
-
     return render_template("emprestimos.html")
 
 
-url = "http://localhost:3000"  # $ criar parametro de ambiente
+url = "https://pi002.herokuapp.com/"  # $ criar parametro de ambiente
 
-alunos = [
-    {"id": "1230258", "nome": "Carlos"},
-    {"id": "1230259", "nome": "Gustavo fsfa"},
-    {"id": "102", "nome": "adasdasd"},
-    {"id": "123", "nome": "Tetse"},
-    {"id": "22222", "nome": "SAO PAULO"},
-    {"id": "22222", "nome": "ACME"},
-    {"id": "9999999", "nome": "ACME"},
-    {"id": "999999999", "nome": "ACME"},
-    {"id": "wvErZ0K", "nome": ""},
-    {"id": "Ugk0NVS", "nome": ""},
-    {"id": "ffQIDkM", "nome": ""},
-    {"id": "Doi8jDc", "nome": ""},
-    {"id": "96565", "nome": "Tropeço"},
-    {"id": "2to1LxO", "nome": ""},
-]
--- static/js/emprestimo.js
+++ static/js/emprestimo.js
@@ -1,33 +1,40 @@
-function busca_imagem() {
-	
+
+
+
+async function busca_imagem() {
+	id_livro = document.getElementById('isbn').value;
+	var url = 'https://pi002.herokuapp.com/livro/';
+	var url_get = url + id_livro + '/?format=json'
+	const response = await fetch(url_get);
+	const jsonData = await response.json();
+	console.log(jsonData);
+	document.getElementById('imagem_livro').src = jsonData.thumb;
+}
+/*
 	console.log('o id do livro é:');
 	id_livro = document.getElementById('isbn').value;
 	console.log(id_livro);
 
-	var url = 'https://pi002.herokuapp.com/livro/3/?format=json';
+	var url = 'https://pi002.herokuapp.com/livro/1/?format=json';
 	//var url_get = url + id_livro + '/?format=json';
 	dados = fetchAsync(url);
-	document.getElementById("imagem_livro").src = dados.thumb
+	console.log('dados')
+	console.log(dados)
+	document.getElementById('imagem_livro').src = dados.thumb;
 }
 async function fetchAsync(url) {
 	const opcao = {
+		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*',
-			"Access-Control-Allow-Credentials" : true
 		},
-		mode:'no-cors'
 	};
 
-	const response = fetch(url, opcao)
-		.then(function(responseData){
-		 console.log(responseData)
-		 return responseData.json()
-		})
-		.then(function(jsonData){
-			console.Console(jsonData)
-			return jsonData;
-
-		})
+	const resposta = fetch(url, opcao)
+		.then(function (responseData) {
+			return responseData.json();
+        })
+         .then(function(jsonData){   
+			return(jsonData)
 		
-}
+}*/
